720fe23dc68ac8139ab7bcfc9f291fdc529a8107
seongho.hong <hsh3592@gmail.com>
2021-08-14 15:45:54 +0900

    Initial Commit

---
+++ UIHosting/UIHosting.h
@@ -0,0 +1,18 @@
+//
+//  UIHosting.h
+//  UIHosting
+//
+//  Created by seongho.hong on 2021/08/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIHosting.
+FOUNDATION_EXPORT double UIHostingVersionNumber;
+
+//! Project version string for UIHosting.
+FOUNDATION_EXPORT const unsigned char UIHostingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIHosting/PublicHeader.h>
+
+
---
+++ UIHostingExample/UIHostingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIHostingExample
+//
+//  Created by seongho.hong on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
